e66726f11e6731395c15318aa00f36be538871b7
Gail Terman <gterman@gmail.com>
2022-11-27 20:29:02 -0500

    two step create dialog closes #30

--- supabase-demo/app/src/app/create-session/create-session-dialog/create-session-dialog.component.ts
+++ supabase-demo/app/src/app/create-session/create-session-dialog/create-session-dialog.component.ts
@@ -1,8 +1,8 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Select, Store } from '@ngxs/store';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Game, NewSession } from '../../interfaces';
 import { GameState } from '../../_store/game.store';
 import { GameSessionActions } from '../../_store/game_session.actions';
@@ -12,8 +12,10 @@ import { GameSessionActions } from '../../_store/game_session.actions';
   templateUrl: './create-session-dialog.component.html',
   styleUrls: ['./create-session-dialog.component.css'],
 })
-export class CreateSessionDialogComponent {
+export class CreateSessionDialogComponent implements OnInit {
   @Select(GameState.games) games$!: Observable<Game[]>;
+  step = 0;
+  gameName$?: Observable<string>;
   createForm = this.fb.group({
     game_id: this.fb.nonNullable.control<number>(-1, Validators.required),
     event_id: this.fb.nonNullable.control(1, Validators.required),
@@ -30,9 +32,24 @@ export class CreateSessionDialogComponent {
     private readonly fb: FormBuilder
   ) {}
 
+  ngOnInit(): void {
+    this.step = 0;
+    this.gameName$ = this.games$.pipe(
+      map(
+        (games) =>
+          games.find((g) => g.game_id === this.createForm.value.game_id)
+            ?.name || 'Unknown game'
+      )
+    );
+  }
+
   createSession() {
     this.store.dispatch(
       new GameSessionActions.Create(this.createForm.value as NewSession)
     );
+  }
+
+  nextStep() {
+    this.step = 1;
   }
 }
